c455c9d4f095ce8fedb738385b08be898d60a237
jwang206 <jwang206@hawk.iit.edu>
2017-11-26 03:00:33 -0600

    organize code | get search directory working

--- app.js
+++ app.js
@@ -80,8 +80,6 @@ app.use('/oauthcall2back', oauth);
 
 
 
-
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
--- routes/index.js
+++ routes/index.js
@@ -40,10 +40,76 @@ router.get('/auth/google/callback',
     res.redirect('/search');
   });
 
-// GET /search
-router.get('/search',(req,res)=>{
-  res.render('search');
-});
+  // Get /search
+  router.get('/search', function(req, res) {
+      res.render('search', {
+          title: 'Search Book'
+      });
+  });
+
+  // listening on the index page
+  router.post('/search', (req, res, next) => {
+      console.log("posting from index page--------------->");
+      var bookname = req.body.bkname;
+      var reqBody;
+      console.log("index: the input is: ");
+      console.log(req.body.bkname);
+      var books = [];
+      var items = {};
+
+      var books_title = [];
+      var books_authors = [];
+      var books_link = [];
+      var books_image = [];
+
+
+      console.log('hihihihihi');
+
+
+      const payload = {
+          url: 'https://www.googleapis.com/books/v1/volumes?q=' + bookname + '&maxResults=40',
+          method: 'GET',
+          headers: {
+              'Accept': 'application/json',
+              'Accept-Charset': 'utf-8',
+          },
+          qs: {
+              maxResults: 40
+          },
+          json: true
+      };
+
+      request(payload, function(err, output, body) {
+          console.log("PAYLOAD URL:", payload.url);
+          console.log(payload);
+          var booksObj = body;
+          reqBody = body;
+          console.log(booksObj.items.length);
+
+          // Get jason data and stores them in arrays
+          for (var i = 0; i < reqBody.items.length; i++) {
+              // console.log(booksObj.items[i].volumeInfo);
+              // console.log(body);
+              books.push(reqBody.items[i].volumeInfo);
+              books_title.push(reqBody.items[i].volumeInfo.title);
+              books_authors.push(reqBody.items[i].volumeInfo.authors);
+              // console.log(books_title[i]);
+          }
+
+          res.render('search', {
+              title: bookname,
+              bookname: bookname,
+              books:books,
+              books_title: books_title,
+              books_authors:books_authors,
+              // books_link: books_link,
+              // books_image:books_image,
+              api_url: function() {
+                  return payload.url;
+              }
+              });
+      });
+  });
 
 
 router.get('/search/:bookname', function(req, res, next) {
@@ -92,76 +158,8 @@ router.post('/search/:bookname', function(req, res, next) {
     });
 });
 
-// when key in the search path
-router.get('/search', function(req, res) {
-    res.render('search', {
-        title: 'Search Book'
-    });
-});
-
-// listening on the index page
-router.post('/search', (req, res, next) => {
-    console.log("posting from index page--------------->");
-    var bookname = req.body.bkname;
-    var reqBody;
-    console.log("index: the input is: ");
-    console.log(req.body.bkname);
-    var books = [];
-    var items = {};
-
-    var books_title = [];
-    var books_authors = [];
-    var books_link = [];
-    var books_image = [];
 
 
-    console.log('hihihihihi');
-
-
-    const payload = {
-        url: 'https://www.googleapis.com/books/v1/volumes?q=' + bookname + '&maxResults=40',
-        method: 'GET',
-        headers: {
-            'Accept': 'application/json',
-            'Accept-Charset': 'utf-8',
-        },
-        qs: {
-            maxResults: 40
-        },
-        json: true
-    };
-
-    request(payload, function(err, output, body) {
-        console.log("PAYLOAD URL:", payload.url);
-        console.log(payload);
-        var booksObj = body;
-        reqBody = body;
-        console.log(booksObj.items.length);
-
-        // Get jason data and stores them in arrays
-        for (var i = 0; i < reqBody.items.length; i++) {
-            // console.log(booksObj.items[i].volumeInfo);
-            // console.log(body);
-            books.push(reqBody.items[i].volumeInfo);
-            books_title.push(reqBody.items[i].volumeInfo.title);
-            books_authors.push(reqBody.items[i].volumeInfo.authors);
-            // console.log(books_title[i]);
-        }
-
-        res.render('search', {
-            title: bookname,
-            bookname: bookname,
-            books:books,
-            books_title: books_title,
-            books_authors:books_authors,
-            // books_link: books_link,
-            // books_image:books_image,
-            api_url: function() {
-                return payload.url;
-            }
-            });
-    });
-});
 
 router.get('/add',(req,res)=>{
   res.send('it works');
