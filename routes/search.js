efa57024f64b11e0e8da4af76ab9e64d540c8755
jwang206 <jwang206@hawk.iit.edu>
2017-11-28 00:54:02 -0600

    trying to add lable for checkbox, but the cursor changes to input selection bar. so I tried to force it to pointer in css, but it still not working

--- routes/search.js
+++ routes/search.js
@@ -3,11 +3,11 @@ var router = express.Router();
 var request = require('request');
 var bodyparser = require('body-parser');
 
-// router.get('/searchbook', function(req, res, next) {
-//     res.render('search', {
-//         title: 'Book Search'
-//     });
-// });
+router.get('/searchbook', function(req, res, next) {
+    res.render('search', {
+        title: 'Book Search'
+    });
+});
 
 
 
